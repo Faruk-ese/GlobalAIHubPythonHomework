aa6631ca8def3cbef9209cf50e74b3f79646ea11
UslAnmaz023 <68063605+UslAnmaz023@users.noreply.github.com>
2020-12-22 15:51:52 +0300

    Add files via upload

---
+++ Untitled14.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a = input("Enter the first value:")
+b = input("Enter the second value:")
+c = input("Enter the third value:")
+d = input("Enter the fourth value:")
+e = input("Enter the fifth value:")
+liste = list()
+liste = [a,b,c,d,e]
+print("{} is a string \n{} is an integer \n{} is a string \n{} is a float \n{} is an integer".format(liste[0],liste[1],liste[2],liste[3],liste[4]))
+
+
+# In[ ]:
+
+
+
+
